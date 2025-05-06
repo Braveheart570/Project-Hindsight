8f495bc8ab9b1b0cb5906603de5caf91fa9db093
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-05-06 19:05:51 -0400

    working proof of concept shader

--- main.lua
+++ main.lua
@@ -9,24 +9,28 @@ function loadFile(file)
     return content
 end
 
-local shader_code2 = loadFile("mask.fs")
-
+local maskShader
 
 local image
 
 local maskCanvas
-local maskShader
+
+
 
 local mainCanvas
+
+local hiddenCanvas --for hidden entities
 
 local player
 
 function love.load()
     love.window.setMode(1200,800)
     image = love.graphics.newImage("map.png")
-    maskShader = love.graphics.newShader(shader_code2)
+    maskShader = love.graphics.newShader(loadFile("mask.fs"))
     maskCanvas = love.graphics.newCanvas();
     mainCanvas = love.graphics.newCanvas();
+    hiddenCanvas = love.graphics.newCanvas();
+
 
     player = {
         x=love.graphics.getWidth()/2,
@@ -56,7 +60,7 @@ function love.draw()
 
     --mask canvas
     love.graphics.setCanvas(maskCanvas)
-    love.graphics.setColor({0.1,0.1,0.1})
+    love.graphics.setColor({0.2,0.2,0.2})
     love.graphics.rectangle("fill",0,0,love.graphics.getWidth(),love.graphics.getHeight())
     love.graphics.setColor(1,1,1)
     love.graphics.circle("fill",player.x,player.y,50)
@@ -74,8 +78,18 @@ function love.draw()
 
     love.graphics.polygon("fill",{player.x,player.y,p1.x,p1.y,p2.x,p2.y})
 
+
+    --hidden canvas
+    love.graphics.setCanvas(hiddenCanvas)
+    love.graphics.setColor({0,0,0})
+    love.graphics.rectangle("fill",0,0,love.graphics.getWidth(),love.graphics.getHeight())
+    love.graphics.setColor({1,0,0})
+    love.graphics.circle("fill",love.graphics.getWidth()/2 + 200, love.graphics.getHeight()/2 + 100,50)
+
+
     --scene canvas
     love.graphics.setCanvas(mainCanvas)
+    love.graphics.setColor({1,1,1})
     love.graphics.draw(image,love.graphics.getWidth()/2 - image:getWidth()/2,love.graphics.getHeight()/2 - image:getHeight()/2)
 
 
@@ -83,6 +97,7 @@ function love.draw()
     love.graphics.setCanvas()
     love.graphics.setShader(maskShader)
     maskShader:send("mask",maskCanvas)
+    maskShader:send("entities",hiddenCanvas)
     love.graphics.draw(mainCanvas)
     love.graphics.setShader()
 
--- mask.fs
+++ mask.fs
@@ -1,10 +1,17 @@
 
 extern Image mask;
+extern Image entities;
 
 vec4 effect(vec4 color,Image image, vec2 uvs, vec2 screen_coords){
 
-    vec4 pixel = Texel(image, uvs);
+    vec4 mapPixel = Texel(image, uvs);
+    vec4 entityPixel = Texel(entities,uvs);
+    vec4 maskPixel = Texel(mask,uvs);
 
-    return pixel * color * Texel(mask,uvs);
+    if(entityPixel != vec4(0,0,0,1) && maskPixel == vec4(1,1,1,1)){
+        return entityPixel * color;
+    }
+
+    return mapPixel * color * maskPixel;
   
 }
