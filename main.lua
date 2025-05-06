023a0f34dcd01d16476fdef468eee998f5773636
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-05-05 21:56:03 -0400

    Simple player view

--- main.lua
+++ main.lua
@@ -13,10 +13,18 @@ local shader_code2 = loadFile("mask.fs")
 
 
 local image
+
 local maskCanvas
 local maskShader
 
 local mainCanvas
+
+local player = {
+    x=love.graphics.getWidth()/2,
+    y=love.graphics.getHeight()/2,
+    r=math.rad(45),
+    speed = 50
+}
 
 function love.load()
     love.window.setMode(1200,800)
@@ -28,6 +36,16 @@ end
 
 
 function love.update(dt)
+
+    if love.keyboard.isDown("d") then
+        player.r = player.r + math.rad(player.speed)*dt
+    elseif love.keyboard.isDown("a") then
+        player.r = player.r - math.rad(player.speed)*dt
+    end
+
+
+    player.vx = math.sin(player.r)
+    player.vy = math.cos(player.r)
 end
 
 
@@ -38,7 +56,19 @@ function love.draw()
     love.graphics.setColor({0.2,0.2,0.2})
     love.graphics.rectangle("fill",0,0,love.graphics.getWidth(),love.graphics.getHeight())
     love.graphics.setColor(1,1,1)
-    love.graphics.polygon("fill",{300,300,600,600,300,600})
+    love.graphics.circle("fill",player.x,player.y,50)
+
+    local p1 = {}
+    local p2 = {}
+    local viewAngle = 40
+
+    p1.x = (math.sin(player.r - math.rad(viewAngle/2)) * 500) + player.x
+    p1.y = (math.cos(player.r - math.rad(viewAngle/2)) * 500) + player.y
+
+    p2.x = (math.sin(player.r + math.rad(viewAngle/2)) * 500) + player.x
+    p2.y = (math.cos(player.r + math.rad(viewAngle/2)) * 500) + player.y
+
+    love.graphics.polygon("fill",{player.x,player.y,p1.x,p1.y,p2.x,p2.y})
 
     --scene canvas
     love.graphics.setCanvas(mainCanvas)
@@ -50,6 +80,7 @@ function love.draw()
     love.graphics.setShader(maskShader)
     maskShader:send("mask",maskCanvas)
     love.graphics.draw(mainCanvas)
+    love.graphics.setShader()
 
 
 
