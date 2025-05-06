0226293ffce39c4ccb9f716899963b16392f8e63
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-05-05 21:28:16 -0400

    masking with a canvas

--- main.lua
+++ main.lua
@@ -13,14 +13,17 @@ local shader_code2 = loadFile("mask.fs")
 
 
 local image
-local maskImage
+local maskCanvas
 local maskShader
+
+local mainCanvas
 
 function love.load()
     love.window.setMode(1200,800)
     image = love.graphics.newImage("map.png")
-    maskImage = love.graphics.newImage("overlay1.png")
     maskShader = love.graphics.newShader(shader_code2)
+    maskCanvas = love.graphics.newCanvas();
+    mainCanvas = love.graphics.newCanvas();
 end
 
 
@@ -29,12 +32,21 @@ end
 
 
 function love.draw()
-    love.graphics.setShader(maskShader)
 
-    maskShader:send("mask",maskImage)
-
-    
+    love.graphics.setCanvas(maskCanvas)
+    love.graphics.setColor({0.2,0.2,0.2})
+    love.graphics.rectangle("fill",0,0,love.graphics.getWidth(),love.graphics.getHeight())
+    love.graphics.setColor(1,1,1)
+    love.graphics.circle("fill",love.graphics.getWidth()/2,love.graphics.getHeight()/2,300)
+    love.graphics.setCanvas(mainCanvas)
     love.graphics.draw(image,love.graphics.getWidth()/2 - image:getWidth()/2,love.graphics.getHeight()/2 - image:getHeight()/2)
+    love.graphics.setCanvas()
+    love.graphics.setShader(maskShader)
+    maskShader:send("mask",maskCanvas)
+    love.graphics.draw(mainCanvas)
+
+
+
     love.graphics.setShader()
 end
 
